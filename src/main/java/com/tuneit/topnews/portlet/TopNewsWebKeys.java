a8efedcbe2bb1a688bf1201d34d0f32a6d04c6b9
ttt <42>
2015-04-02 18:20:16 +0400

    WebKeys file added

---
+++ src/main/java/com/tuneit/topnews/portlet/TopNewsWebKeys.java
@@ -0,0 +1,10 @@
+package com.tuneit.topnews.portlet;
+
+public interface TopNewsWebKeys {
+    public static final String FOLDER_ID_PARAM_NAME = "folderId";
+    public static final String REPOSITORY_ID_PARAM_NAME = "repositoryId";
+    public static final String CHANGE_LOG_PARAM_NAME = "changeLog";
+    public static final String REDIRECT_URL_PARAM_NAME = "redirectURL";
+    public static final String NAME_PARAM_NAME = "name";
+    public static final String UPLOAD_FIELD_PARAM_NAME = "uploadField";
+}
